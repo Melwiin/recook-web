1ac9a0ed3c0ab1a70520d9609e523d99b750feb7
Melwin Kramer <melwin.kramer@gmail.com>
2022-09-14 11:08:28 +0200

    implemented react router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import RecipeScroll from "./components/RecipeScroll/RecipeScroll";
-import "./App.css"
 import RecipeSearchBar from "./components/RecipeSearchBar/RecipeSearchBar";
+
+import "./App.css"
+
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
 
     return (
-        <div className="App">
-            <header className="AppHeader">
-                <div className="header-title"><h1>ReCook</h1></div>
-                <RecipeSearchBar/>
-                <div>
-                    <button>+ Recipe</button>
-                </div>
-            </header>
-            {/*<RecipeCreator/>*/}
-            <RecipeScroll/>
-        </div>
+        <Router>
+            <div className="App">
+                <header className="AppHeader">
+                    <div className="header-title"><h1>ReCook</h1></div>
+                    <RecipeSearchBar/>
+                    <div>
+                        <button>+ Recipe</button>
+                    </div>
+                </header>
+                {/*<RecipeCreator/>*/}
+                <Routes>
+                    <Route path="/" element={<RecipeScroll/>} />
+                    <Route path="*" element={<h2>404</h2>}/>
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
