9cc05a33deb23e131f97e543f590f1442c6eac98
Melwin Kramer <melwin.kramer@gmail.com>
2022-09-14 10:41:20 +0200

    added search bar suggestions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import RecipeScroll from "./components/RecipeScroll/RecipeScroll";
 import "./App.css"
-import RecipeCreator from "./components/RecipeCreator/RecipeCreator";
 import RecipeSearchBar from "./components/RecipeSearchBar/RecipeSearchBar";
 
 function App() {
@@ -8,8 +7,11 @@ function App() {
     return (
         <div className="App">
             <header className="AppHeader">
-                <h1>ReCook</h1>
+                <div className="header-title"><h1>ReCook</h1></div>
                 <RecipeSearchBar/>
+                <div>
+                    <button>+ Recipe</button>
+                </div>
             </header>
             {/*<RecipeCreator/>*/}
             <RecipeScroll/>
--- src/components/RecipeSearchBar/RecipeSearchBar.js
+++ src/components/RecipeSearchBar/RecipeSearchBar.js
@@ -1,30 +1,38 @@
 import "./RecipeSearchBar.css"
 import SearchIcon from "../../assets/search_icon.svg";
+import {useState} from "react";
+import axios from "axios";
 
-export default function RecipeSearchBar(args) {
+export default function RecipeSearchBar() {
+    const [searchValue, setSearchValue] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
 
-    const suggestions = [
-        "Thai Curry",
-        "Pizza Teig",
-        "Chilli con Carne",
-        "Rotes Curry",
-        "Desert",
-        "Bananen sorbe",
-        "Chilli sin Carne",
-    ]
+    const fetchSuggestions = (query) => {
+        axios.get(process.env.REACT_APP_API_DOMAIN + "/suggestions?q=" + query)
+            .then(res => {
+                setSuggestions(res.data);
+            })
+    }
 
     return(
         <div className="search-bar">
             <img src={SearchIcon} alt="search icon"/>
-            <input type="text" placeholder="Search your favorite recipe"/>
-            <div className="search-suggestion-box">
+            <input type="text" placeholder="Search your favorite recipe" value={searchValue} onChange={event => {
+                setSearchValue(event.target.value);
+                fetchSuggestions(event.target.value);
+            }}/>
+            {searchValue.length ? (<div className="search-suggestion-box">
                 <ul className="search-suggestion-list">
-                    {!suggestions.length ? <h3>Loading...</h3> :
-                        suggestions.map(s => {
-                      return <li>{s}</li>
-                    })}
+                    {!suggestions.length ? <h4>No recipe found :(</h4> :
+                        suggestions.filter((str) => {
+                            return str.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase());
+                        })
+                            .map(s => {
+                                return <li key={s}>{s}</li>
+                            })
+                    }
                 </ul>
-            </div>
+            </div>) : null}
         </div>
     );
 }
